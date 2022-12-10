9e69e55e57a2add559cf2d5680776f141ae0bcef
zxcv13579 <j0910290373@gmail.com>
2022-12-10 20:02:39 +0800

    指定 todoList id 刷新

--- src/pages/AsyncTodoListPage.jsx
+++ src/pages/AsyncTodoListPage.jsx
@@ -1,22 +1,30 @@
-import { useCallback, Suspense, useState } from "react";
+import { Suspense, useState } from "react";
 import TodoInfo from "../components/async/TodoInfo";
 import TodoInfoWithoutSuspense from "../components/async/TodoInfoWithoutSuspense";
-import TodoInput from "../components/async/TodoInput";
 import { useSetRecoilState } from "recoil";
 import { todoRequestIdState } from "../states/async";
 
-const AsyncTodoListPage = () => {
+const useRefreshTodoInfo = (id) => {
   // 這裡的 id 會是唯一不會重複的，每一個不同的 id 都表示的是 Atom Family 中不同的 Atom
-  const setTodoRequestId = useSetRecoilState(todoRequestIdState("1"));
-
-  const refreshTodoInfo = useCallback(() => {
+  const setTodoRequestId = useSetRecoilState(todoRequestIdState(id));
+  return () => {
     setTodoRequestId((prev) => prev + 1);
-  }, [setTodoRequestId]);
+  };
+};
+
+const AsyncTodoListPage = () => {
+  const [id, setId] = useState("1");
+
+  const refreshTodoInfo = useRefreshTodoInfo(id);
 
   return (
     <div>
-      <button onClick={refreshTodoInfo}>強制刷新 todo 1</button>
-      <TodoInput />
+      <button onClick={refreshTodoInfo}>強制刷新 todo {id}</button>
+      <select value={id} onChange={(e) => setId(e.target.value)}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
       <Suspense fallback="loading...">
         <TodoInfo id="1" />
         <TodoInfo id="2" />
